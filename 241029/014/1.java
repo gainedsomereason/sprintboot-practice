50c4718098f4fb57192f7a70aadd0e44345f776f
gainedsomereason <1368444910@qq.com>
2025-03-11 09:24:30 +0800

    Create 1.java

---
+++ 241029/014/1.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZypjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZypjApplication.class, args);
+	}
+
+}
+/**
+ai语句实例：
+	详细介绍@Test注解在Springboot中的使用
+	
+Spring Boot DevTools
+Lombok
+Spring Web
+MySQL Driver
+Sprint Boot JPA
+
+*/
